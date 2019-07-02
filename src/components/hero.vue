cc379b3c2cbc72043825438e8403075eae64c8e2
amradelata <amradelata@gmail.com>
2019-07-02 13:26:46 +0200

    vue-app-ecommerce-bootstrap

---
+++ src/components/hero.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="hero">
+        <div class="container">
+            <div class="row h-100 justify-content align-items-center">
+                <div class="col-md-7">
+                    <div class="img">
+                        
+                        <img src="img/e-commerce.svg" class="img-fluid" alt="Responsive image">
+                    </div>
+                </div>
+                <div class="col-md-5">
+                    <div class="hero-content">
+                        <h1 class="hero-title">
+                            Vue Shop For You 
+                        </h1>
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore repellat voluptatem earum natus exercitationem voluptatibus, beatae eaque, laborum aut possimus ut sequi illo culpa porro temporibus delectus! Aperiam, voluptate earum.
+                        </p>
+                        <div class="hero-btn mt-5">
+                            <button class="btn custom-btn btn-info mr-4">Explore</button>    
+                            <button class="btn custom-btn btn-outline-secondary">products</button>
+                        </div> 
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+</template>
+
+
+<style scoped>
+.hero{
+    position: absolute;
+    top:50%;
+    left:50%;
+    transform: translate(-50%, -50%)
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import BootstrapVue from 'bootstrap-vue/dist/bootstrap-vue.esm';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
+import './assets/app.css'
+import jquery from 'jquery'
+import 'popper.js'
+
+
+
+Vue.component('navpar', require('./components/navpar.vue').default);
 
 Vue.config.productionTip = false
 
